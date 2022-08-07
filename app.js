3ef481f5bd307938a0a0900c1ead3ba74283f9a9
limchaekyung <corud505@naver.com>
2022-08-08 00:02:29 +0900

    moveTo and lineTo

--- app.js
+++ app.js
@@ -5,13 +5,23 @@ canvas.height = 800;
 
 // ctx.fillRect(50, 50, 100, 200); // x좌표, y좌표, 가로, 세로
 
-ctx.rect(50, 50, 100, 100); // 선 그음
-ctx.rect(150, 150, 100, 100);
-ctx.rect(250, 250, 100, 100);
-ctx.fill(); // 색 채움
+/** paths */
+// ctx.rect(50, 50, 100, 100); // 선 그음
+// ctx.rect(150, 150, 100, 100);
+// ctx.rect(250, 250, 100, 100);
+// ctx.fill(); // 색 채움
 
-ctx.beginPath();    // 새 경로
-ctx.rect(350, 350, 100, 100);
-ctx.rect(450, 450, 100, 100);
-ctx.fillStyle = "red";
+// ctx.beginPath();    // 새 경로
+// ctx.rect(350, 350, 100, 100);
+// ctx.rect(450, 450, 100, 100);
+// ctx.fillStyle = "red";
+// ctx.fill();
+
+/** moveTo and lineTo */
+ctx.moveTo(50, 50); // x좌표, y좌표
+ctx.lineTo(150, 50);    // x좌표 이동, y좌표 이동
+ctx.lineTo(150, 150);
+ctx.lineTo(50, 150);
+ctx.lineTo(50, 50);
 ctx.fill();
+
