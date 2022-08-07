e80baf729076b8200986f9e1be4370eb0b6c2c96
limchaekyung <corud505@naver.com>
2022-08-07 23:47:05 +0900

    first drawing

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-alert('hello')
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext("2d");
+canvas.width = 800;
+canvas.height = 800;
+
+ctx.fillRect(50, 50, 100, 200); // x좌표, y좌표, 가로, 세로
