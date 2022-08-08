e7fbb6f88c74f8eb048c8f0502e04da299bddb27
limchaekyung <corud505@naver.com>
2022-08-08 23:17:33 +0900

    drawing project2

--- app.js
+++ app.js
@@ -26,12 +26,24 @@ canvas.height = 800;
 // ctx.fill();
 
 /** house */
-ctx.fillRect(200, 200, 50, 200);
-ctx.fillRect(400, 200, 50, 200);
-ctx.lineWidth = 2;
-ctx.fillRect(300, 300, 50, 100);
-ctx.fillRect(200, 200, 200, 20);
-ctx.moveTo(200, 200);
-ctx.lineTo(325, 100);
-ctx.lineTo(450, 200);
+// ctx.fillRect(200, 200, 50, 200);
+// ctx.fillRect(400, 200, 50, 200);
+// ctx.lineWidth = 2;
+// ctx.fillRect(300, 300, 50, 100);
+// ctx.fillRect(200, 200, 200, 20);
+// ctx.moveTo(200, 200);
+// ctx.lineTo(325, 100);
+// ctx.lineTo(450, 200);
+// ctx.fill();
+
+/** Person */
+ctx.fillRect(210 - 40, 200 - 20, 15, 100);
+ctx.fillRect(350 - 40, 200 - 20, 15, 100);
+ctx.fillRect(260 - 40, 200 - 20, 60, 200);
+ctx.arc(250, 100, 50, 0, 2 * Math.PI);    // x좌표, y좌표, 원의 크기, 원의 시작점, 원의 끝점
+ctx.fill();
+ctx.beginPath();
+ctx.fillStyle = "white";
+ctx.arc(260 + 10, 80, 6, Math.PI, 2 * Math.PI);
+ctx.arc(220 + 10, 80, 6, Math.PI, 2 * Math.PI);
 ctx.fill();
