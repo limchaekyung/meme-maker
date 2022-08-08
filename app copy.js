db4fa53e60b15c8fe655386fef4db1d605233ea5
limchaekyung <corud505@naver.com>
2022-08-08 23:30:12 +0900

    Painting Lines

---
+++ app copy.js
@@ -0,0 +1,49 @@
+// const canvas = document.querySelector('canvas');
+// const ctx = canvas.getContext("2d");
+// canvas.width = 800;
+// canvas.height = 800;
+
+// // ctx.fillRect(50, 50, 100, 200); // x좌표, y좌표, 가로, 세로
+
+// /** paths */
+// // ctx.rect(50, 50, 100, 100); // 선 그음
+// // ctx.rect(150, 150, 100, 100);
+// // ctx.rect(250, 250, 100, 100);
+// // ctx.fill(); // 색 채움
+
+// // ctx.beginPath();    // 새 경로
+// // ctx.rect(350, 350, 100, 100);
+// // ctx.rect(450, 450, 100, 100);
+// // ctx.fillStyle = "red";
+// // ctx.fill();
+
+// /** moveTo and lineTo */
+// // ctx.moveTo(50, 50); // x좌표, y좌표
+// // ctx.lineTo(150, 50);    // x좌표 이동, y좌표 이동
+// // ctx.lineTo(150, 150);
+// // ctx.lineTo(50, 150);
+// // ctx.lineTo(50, 50);
+// // ctx.fill();
+
+// /** house */
+// // ctx.fillRect(200, 200, 50, 200);
+// // ctx.fillRect(400, 200, 50, 200);
+// // ctx.lineWidth = 2;
+// // ctx.fillRect(300, 300, 50, 100);
+// // ctx.fillRect(200, 200, 200, 20);
+// // ctx.moveTo(200, 200);
+// // ctx.lineTo(325, 100);
+// // ctx.lineTo(450, 200);
+// // ctx.fill();
+
+// /** Person */
+// // ctx.fillRect(210 - 40, 200 - 20, 15, 100);
+// // ctx.fillRect(350 - 40, 200 - 20, 15, 100);
+// // ctx.fillRect(260 - 40, 200 - 20, 60, 200);
+// // ctx.arc(250, 100, 50, 0, 2 * Math.PI);    // x좌표, y좌표, 원의 크기, 원의 시작점, 원의 끝점
+// // ctx.fill();
+// // ctx.beginPath();
+// // ctx.fillStyle = "white";
+// // ctx.arc(260 + 10, 80, 6, Math.PI, 2 * Math.PI);
+// // ctx.arc(220 + 10, 80, 6, Math.PI, 2 * Math.PI);
+// // ctx.fill();
--- app.js
+++ app.js
@@ -1,49 +1,30 @@
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext("2d");
+
 canvas.width = 800;
 canvas.height = 800;
+ctx.lineWidth = 2;
 
-// ctx.fillRect(50, 50, 100, 200); // x좌표, y좌표, 가로, 세로
+const colors = [
+    "#c56cf0",
+    "#ffb8b8",
+    "#ff3838",
+    "#ff9f1a",
+    "#ff9f1a",
+    "#fff200",
+    "#32ff7e",
+    "#7efff5",
+    "#18dcff",
+    "#7d5fff",
+    "#4b4b4b",
+]
+function onClick(event) {
+    ctx.beginPath();
+    ctx.moveTo(400, 400);
+    const color = colors[Math.floor(Math.random() * colors.length)];
+    ctx.strokeStyle = color;
+    ctx.lineTo(event.offsetX, event.offsetY);
+    ctx.stroke();
+}
 
-/** paths */
-// ctx.rect(50, 50, 100, 100); // 선 그음
-// ctx.rect(150, 150, 100, 100);
-// ctx.rect(250, 250, 100, 100);
-// ctx.fill(); // 색 채움
-
-// ctx.beginPath();    // 새 경로
-// ctx.rect(350, 350, 100, 100);
-// ctx.rect(450, 450, 100, 100);
-// ctx.fillStyle = "red";
-// ctx.fill();
-
-/** moveTo and lineTo */
-// ctx.moveTo(50, 50); // x좌표, y좌표
-// ctx.lineTo(150, 50);    // x좌표 이동, y좌표 이동
-// ctx.lineTo(150, 150);
-// ctx.lineTo(50, 150);
-// ctx.lineTo(50, 50);
-// ctx.fill();
-
-/** house */
-// ctx.fillRect(200, 200, 50, 200);
-// ctx.fillRect(400, 200, 50, 200);
-// ctx.lineWidth = 2;
-// ctx.fillRect(300, 300, 50, 100);
-// ctx.fillRect(200, 200, 200, 20);
-// ctx.moveTo(200, 200);
-// ctx.lineTo(325, 100);
-// ctx.lineTo(450, 200);
-// ctx.fill();
-
-/** Person */
-ctx.fillRect(210 - 40, 200 - 20, 15, 100);
-ctx.fillRect(350 - 40, 200 - 20, 15, 100);
-ctx.fillRect(260 - 40, 200 - 20, 60, 200);
-ctx.arc(250, 100, 50, 0, 2 * Math.PI);    // x좌표, y좌표, 원의 크기, 원의 시작점, 원의 끝점
-ctx.fill();
-ctx.beginPath();
-ctx.fillStyle = "white";
-ctx.arc(260 + 10, 80, 6, Math.PI, 2 * Math.PI);
-ctx.arc(220 + 10, 80, 6, Math.PI, 2 * Math.PI);
-ctx.fill();
+canvas.addEventListener("mousemove", onClick);
