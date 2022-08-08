c3c01514fe3b1b42811a70b275861f9ed99bff57
limchaekyung <corud505@naver.com>
2022-08-09 00:35:30 +0900

    Line Width

--- app.js
+++ app.js
@@ -1,55 +1,38 @@
+const lineWidth = document.getElementById('line-width');
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext("2d");
 
 canvas.width = 800;
 canvas.height = 800;
-ctx.lineWidth = 2;
-
-// const colors = [
-//     "#c56cf0",
-//     "#ffb8b8",
-//     "#ff3838",
-//     "#ff9f1a",
-//     "#ff9f1a",
-//     "#fff200",
-//     "#32ff7e",
-//     "#7efff5",
-//     "#18dcff",
-//     "#7d5fff",
-//     "#4b4b4b",
-// ]
-
-// function onClick(event) {
-//     ctx.beginPath();
-//     ctx.moveTo(400, 400);
-//     const color = colors[Math.floor(Math.random() * colors.length)];
-//     ctx.strokeStyle = color;
-//     ctx.lineTo(event.offsetX, event.offsetY);
-//     ctx.stroke();
-// }
-
-// canvas.addEventListener("mousemove", onClick);
+ctx.lineWidth = lineWidth.value;
 
 let isPainting = false;
 
 function onMove(event) {
     if (isPainting) {
-        ctx.lineTo(event.offsetX, event.offSetY);
+        ctx.lineTo(event.offsetX, event.offsetY);
         ctx.stroke();
         return;
     }
     ctx.moveTo(event.offsetX, event.offsetY);
 }
 
-function onMouseDown() {
+function startPainting() {
     isPainting = true;
 }
 
-function onMouseUp() {
+function cancelPainting() {
     isPainting = false;
+    ctx.beginPath();
+}
+
+function onLineWidthChange(event) {
+    ctx.lineWidth = event.target.value;
 }
 
 canvas.addEventListener("mousemove", onMove);
-canvas.addEventListener("mousedown", onMouseDown);
-canvas.addEventListener("mouseup", onMouseUp);
-canvas.addEventListener("mouseleave", onMouseUp);
+canvas.addEventListener("mousedown", startPainting);
+canvas.addEventListener("mouseup", cancelPainting);
+canvas.addEventListener("mouseleave", cancelPainting);
+
+lineWidth.addEventListener("change", onLineWidthChange);
